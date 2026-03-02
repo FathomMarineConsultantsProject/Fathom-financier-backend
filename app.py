923f4bf8672bbf6a3a5ffeff236dc4f2bf33ab00
Anmol-fmc <project@fathommarineconsultants.com>
2026-03-02 13:00:34 +0530

    database

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from src.db.database import engine, Base
 from src.routes.employeeRoutes import router as employee_router
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.exc import OperationalError
+
 
 
 app = FastAPI(title="Fathom Employee API")
@@ -13,7 +15,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-Base.metadata.create_all(bind=engine)
+
+
+try:
+    Base.metadata.create_all(bind=engine)
+except OperationalError as e:
+    print("DB not ready:", e)
 
 app.include_router(employee_router)
 
